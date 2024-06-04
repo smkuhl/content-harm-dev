15bea1e728b53d3fbdc73ee07535f2c151849c77
Lin Qiu <qiulin0128@gmail.com>
2024-06-04 15:31:22 -0700

    new dataset

--- SurveyData.py
+++ SurveyData.py
@@ -9,7 +9,7 @@ from st_files_connection import FilesConnection
 
 # This library handles the distribution & management of survey data.
 group_size = 6
-tweet_dataset_filepath = "misinfo-harm/emoji.csv"
+tweet_dataset_filepath = "misinfo-harm/tweet_data_formatted_pt3.csv"
 
 def get_tweet_set_random():
     conn = st.connection('gcs', type=FilesConnection)
